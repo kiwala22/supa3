3ec104b58ed2a51154816f457fa2c02fc49fb9dd
martin kiwala <martinkiwala4@gmail.com>
2019-06-10 17:22:59 +0300

    still fixing syntax error in broadcast worker

--- app/workers/broadcast_worker.rb
+++ app/workers/broadcast_worker.rb
@@ -11,7 +11,7 @@ class BroadcastWorker
       contacts = 0
       @gamers.each do |gamer|
          #Send Marketing Message from external reusable library
-         self.process_sms_now(receiver: nil, content: nil, sender_id: nil, args = {})
+         self.process_sms_now(receiver: nil, content: nil, sender_id: nil, args: {})
          if process_sms_now(receiver: gamer.phone_number, content: @broadcast.message, sender_id: ENV['DEFAULT_SENDER_ID'])
             contacts = (contacts + 1)
          end
