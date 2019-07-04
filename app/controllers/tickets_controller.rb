a588a67a28b043eb9396101275ad6d71b7ba77c6
martin kiwala <martinkiwala4@gmail.com>
2019-07-04 11:13:11 +0300

    skiping devise authenticity in tickets controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,6 @@
 class TicketsController < ApplicationController
    before_action :authenticate_user!, except: [:create]
+   skip_before_action :verify_authenticity_token
    load_and_authorize_resource
 
    def index
