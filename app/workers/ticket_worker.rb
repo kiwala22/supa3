40a19ee22a7a4ecd2050e484720b507399c01a5d
martin kiwala <martinkiwala4@gmail.com>
2019-07-05 13:24:26 +0300

    created environment variable for game

--- app/workers/ticket_worker.rb
+++ app/workers/ticket_worker.rb
@@ -16,7 +16,7 @@ class TicketWorker
          ticket = gamer.tickets.new(phone_number: gamer.phone_number, data: data, amount: amount.to_i, reference: reference)
          if ticket.save
             #Send SMS with confirmation
-            message_content = "Thank you for playing #Game. You have played #{data} entered in to #{draw_time} draw. Ticket: #{reference}"
+            message_content = "Thank you for playing #{ENV['GAME']}. You have played #{data} entered in to #{draw_time} draw. Ticket: #{reference}"
             SendSMS.process_sms_now(receiver: gamer.phone_number, content: message_content, sender_id: ENV['DEFAULT_SENDER_ID'])
          end
 
@@ -26,7 +26,7 @@ class TicketWorker
          ticket = gamer.tickets.new(phone_number: gamer.phone_number, data: random_data, amount: amount.to_i, reference: reference)
          if ticket.save
             #Send SMS with the confirmation and random number
-            message_content = "Thank you for playing #Game. Input was incorrect and we have picked #{random_data} for you entered in to #{draw_time} draw. Ticket: #{reference}"
+            message_content = "Thank you for playing #{ENV['GAME']}. Input was incorrect and we have picked #{random_data} for you entered in to #{draw_time} draw. Ticket: #{reference}"
             SendSMS.process_sms_now(receiver: gamer.phone_number, content: message_content, sender_id: ENV['DEFAULT_SENDER_ID'])
          end
       end
