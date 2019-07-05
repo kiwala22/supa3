00f41f6382e22f0ee27d1a855944bac6bda463d2
martin kiwala <martinkiwala4@gmail.com>
2019-07-05 12:48:03 +0300

    url correction in send_sms file

--- lib/send_sms.rb
+++ lib/send_sms.rb
@@ -7,20 +7,19 @@ module SendSMS
    def self.process_sms_now(receiver:, content:, sender_id: nil, args: {})
 
       if args[:append_to].present?
-         content = content + args[:append_to]
+         content = content+args[:append_to]
       end
 
       content = CGI.escape(content)
-
       parameters = []
-      parameters << "to = #{receiver} " if receiver.present?
-      parameters << "message = #{content} " if content.present?
-      parameters << "from = #{sender_id} " if sender_id.present?
+      parameters << "to=#{receiver}" if receiver.present?
+      parameters << "message=#{content}" if content.present?
+      parameters << "from=#{sender_id}" if sender_id.present?
       parameters << "token=#{ENV['SMS_API_TOKEN']}"
 
       message_params = parameters.join("&")
 
-      message_url = ENV['SMS_BASE_URL'] + message_params
+      message_url = "#{ENV['SMS_BASE_URL']}"+"#{message_params}"
 
       response = HTTParty.get(message_url)
 
