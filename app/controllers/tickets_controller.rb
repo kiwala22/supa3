934a49d20e42455160777ede4b0ffb3d71a30b73
martin kiwala <martinkiwala4@gmail.com>
2019-07-05 11:47:14 +0300

    args correction

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -10,15 +10,15 @@ class TicketsController < ApplicationController
    end
 
    def new
-
-
    end
 
    def create
       #pick api data and respond, push the data to library through a worker
       if params[:phone_number].present? & params[:data].present? & params[:amount].present?
-         args = [phone_number: params[:phone_number], data: params[:data], amount: params[:amount]]
-         TicketWorker.perform_async(args)
+         phone_number = params[:phone_number]
+         data = params[:data]
+         amount = params[:amount]
+         TicketWorker.perform_async(phone_number: phone_number, data: data, amount: amount)
          respond_to do |format|
             format.html {render body: nil}
             format.json { render status: 200}
