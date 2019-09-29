e95e4d50410f880ed1ba843d51b5b22547a71795
martin kiwala <martinkiwala4@gmail.com>
2019-09-29 10:28:46 +0300

    still to look at the draw query in comparisons controller

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -2,13 +2,13 @@ class ComparisonsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @reference = Draw.where("draw_time > ? AND draw_time <= ?", Date.today.to_time, Time.now).sum(:ticket_count)
     ((Date.today - 30)..Date.today).each do |date|
       @date = date
-      @draw = Draw.where("draw_time > ? AND draw_time <= ?" date.to_time, Time.now)
-      ref = @draw.first
-      #still to implement ticket changes
+      @draw = Draw.where("draw_time > ? AND draw_time <= ?", date.to_time, Time.now)
       @tickets = @draw.sum(:ticket_count)
       @users = @draw.sum(:users)
+      @ticket_change = @tickets - @reference
     end
   end
 end
