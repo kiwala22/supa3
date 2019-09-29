ac6f0c380d0939e5850c3c422b1333ef8a006530
martin kiwala <martinkiwala4@gmail.com>
2019-09-29 18:09:59 +0300

    change in comparisons controller

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -5,7 +5,7 @@ class ComparisonsController < ApplicationController
     @reference = Draw.where("draw_time > ? AND draw_time <= ?", Date.today.to_time, Time.now).sum(:ticket_count)
     ((Date.today - 30)..Date.today).each do |date|
       @date = date
-      @draw = Draw.where("draw_time > ? AND draw_time <= ?", date.to_time, Time.now)
+      @draw = Draw.where("draw_time > ? AND draw_time <= ?", date.beginning_of_day, date.end_of_day)
       @tickets = @draw.sum(:ticket_count)
       @users = @draw.sum(:users)
       @ticket_change = @tickets - @reference
