7b89f22fc80ce09d7f58095806cbb8adca385cf4
kiwala22 <martinkiwala4@gmail.com>
2020-01-19 15:10:37 +0300

    added validation on collections model

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,6 +1,8 @@
 class Collection < ApplicationRecord
 	validates :transaction_id, uniqueness: true
 	validates :ext_transaction_id, uniqueness: true
+	validates_length_of :phone_number, :is => 12, :message => "number should be 12 digits long."
+	validates_format_of :phone_number, :with => /\A[256]/, :message => "number should start with 256."
 	before_create :generate_references, on: [ :create ]
 
 	private
@@ -17,7 +19,7 @@ class Collection < ApplicationRecord
 			break self.resource_id = resource_id unless Collection.where(resource_id: resource_id).exists?
 
 		end
-		
-		
+
+
 	end
 end
