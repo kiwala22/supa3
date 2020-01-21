8160d94d63d762095c3481e5b8cb7688e11a3f29
kiwala22 <martinkiwala4@gmail.com>
2020-01-21 16:25:30 +0300

    change in mtn specs

--- spec/controllers/confirmation/mtn_uganda_controller_spec.rb
+++ spec/controllers/confirmation/mtn_uganda_controller_spec.rb
@@ -3,13 +3,18 @@ require 'rails_helper'
 RSpec.describe Confirmation::MtnUgandaController, type: :controller do
 
 	describe "POST create" do
-		it " returns status success for well formed request" do
-			xml = "<?xml version='1.0' encoding='UTF-8'?><ns4:paymentrequest xmlns:ns4='http://www.ericsson.com/em/emm//serviceprovider/v1_0/backend'><transactionid>266</transactionid><accountholderid>ID:256776582036/MSISDN</accountholderid><receivingfri>FRI:l1pbr18000001554@stanflexy.sp1/SP</receivingfri><amount><amount>15000.00</amount><currency>UGX</currency></amount><message>275</message><extension/></ns4:paymentrequest>"
-			request.env['content_type'] = 'application/xml'
-  			request.env['RAW_POST_DATA'] =  xml
-  			post :create
-  			expect(response.body).to eq(200)
+		it "returns status success for well formed request" do
+				uri = URI.parse "http://localhost:3000/confirmation/mtn"
+				xml = "<?xml version='1.0' encoding='UTF-8'?><ns4:paymentrequest xmlns:ns4='http://www.ericsson.com/em/emm//serviceprovider/v1_0/backend'><transactionid>266</transactionid><accountholderid>ID:256776582036/MSISDN</accountholderid><receivingfri>FRI:l1pbr18000001554@stanflexy.sp1/SP</receivingfri><amount><amount>15000.00</amount><currency>UGX</currency></amount><message>275</message><extension/></ns4:paymentrequest>"
+				request = Net::HTTP::Post.new uri
+				request.body = xml
+				request.content_type = 'application/xml'
+				response = Net::HTTP.new(uri.host, uri.port).start { |http| http.request request }
+				expect(response.code).to eq('200')
 
+				#check response body
+				result = Hash.from_xml(response.body)
+				puts result
 		end
 
 		# it "returns failed for similar or duplicate EXT IDS" do
@@ -31,7 +36,7 @@ RSpec.describe Confirmation::MtnUgandaController, type: :controller do
 		# 	request.env['content_type'] = 'application/xml'
   # 			request.env['RAW_POST_DATA'] =  "<?xml version=1.0' encoding='UTF-8'?><ns4:paymentrequest xmlns:ns4='http://www.ericsson.com/em/emm//serviceprovider/v1_0/backend'><transactionid>2637538</transactionid><accountholderid>ID:256779999719/MSISDN</accountholderid><receivingfri>FRI:l1pbr18000001554@stanflexy.sp1/SP</receivingfri><amount><amount>15000.00</amount><currency>UGX</currency></amount><message>256890, 8901</message><extension /></ns4:paymentrequest>"
   # 			post :create
-			
+
 		# end
 
 		# it "saves collection when response is received" do
