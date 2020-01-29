2643cfa6760f9e56cebfaa8859c830a993f1de68
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-01-29 16:20:44 +0300

    fixed cert path

--- lib/mobile_money/mtn_ecw.rb
+++ lib/mobile_money/mtn_ecw.rb
@@ -95,7 +95,7 @@ module MobileMoney
 			request.body = req_xml
 			http.use_ssl = true
 			http.ssl_version = :TLSv1_2
-			http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+			http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 			http.cert = OpenSSL::X509::Certificate.new(File.read(Rails.root.join("config/134_209_22_183.crt")))
 			http.ca_file = "#{Rails.root.join("config/m3_external_cert_CA.crt")}"
 			res = http.request(request)
