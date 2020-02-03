4a1d0db65a69aa8b41ee432f0c883172b04d932f
kiwala22 <martinkiwala4@gmail.com>
2020-02-03 14:03:22 +0300

    added new balances in collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,10 +1,18 @@
 class CollectionsController < ApplicationController
   before_action :authenticate_user!
 
+  require 'mobile_money/mtn_ecw'
+  require 'mobile_money/airtel_uganda'
+
   def index
     authorize! :index, :collection, :message => "You are not allowed to view this page..."
     @q = Collection.all.ransack(params[:q])
     @collections = @q.result(distinct: true).order("created_at DESC").page params[:page]
     @search_params = params[:q]
+
+    @mtn_collections = MobileMoney::MtnEcw.get_collection_balance
+    @mtn_payouts = MobileMoney::MtnEcw.get_payout_balance
+    @airtel_collections = 0
+    @airtel_payouts = 0
   end
 end
