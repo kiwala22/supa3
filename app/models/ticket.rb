eeb09deb01b58566378439ea62021f1638277f7c
kiwala22 <martinkiwala4@gmail.com>
2020-02-14 14:14:42 +0300

    removed gamer id from the fields downloaded from the csv

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,7 +5,7 @@ class Ticket < ApplicationRecord
 
    def self.to_csv
      CSV.generate do |csv|
-       column_names = %w(id first_name last_name phone_number reference created_at)
+       column_names = %w(first_name last_name phone_number reference created_at)
        csv << column_names
        all.each do |result|
          csv << result.attributes.values_at(*column_names)
