91e96e139cac9b543b5a0e26264b3b1d576b5d6a
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-02-16 10:38:16 +0300

    mailer not finished

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -6,19 +6,21 @@ class Collection < ApplicationRecord
 	validates_format_of :phone_number, :with => /\A[256]/, :message => "number should start with 256."
 	before_create :generate_references, on: [ :create ]
 
-	# require 'csv'
-	#
-	# def self.send_daily_report
-	# 	collections = Collection.where("created_at <= ? AND created_at >= ?", Date.yesterday.end_of_day, Date.yesterday.beginning_of_day)
-	# 	collection_csv = CSV.generate do |csv|
-	# 		csv << ["Id","Ext Transaction","Transaction","Currency","Amount","Phone Number","Status","Created At","Updated At","Network"]
-	# 		collections.each do |collection|
-	# 			csv << [ collection.id, collection.ext_transaction_id, collection.transaction_id, collection.currency, collection.amount, collection.phone_number, collection.status, collection.created_at, collection.updated_at, collection.network  ]
-	# 		end
-	# 	end
-	#
-	# 	#call the mailer for deivery
-	# end
+	require 'csv'
+
+	def self.send_daily_report
+		collections = Collection.where("created_at <= ? AND created_at >= ?", Date.yesterday.end_of_day, Date.yesterday.beginning_of_day)
+		collection_csv = CSV.generate do |csv|
+			csv << ["Id","Ext Transaction","Transaction","Currency","Amount","Phone Number","Status","Created At","Updated At","Network"]
+			collections.each do |collection|
+				csv << [ collection.id, collection.ext_transaction_id, collection.transaction_id, collection.currency, collection.amount, collection.phone_number, collection.status, collection.created_at, collection.updated_at, collection.network  ]
+			end
+		end
+
+		#call the mailer for deivery
+		CollectionsMailer.daily_report(collection_csv).deliver_now
+
+	end
 
 	private
 	def generate_references
--- app/models/disbursement.rb
+++ app/models/disbursement.rb
@@ -2,11 +2,20 @@ class Disbursement < ApplicationRecord
 	audited
 	validates :transaction_id, uniqueness: true
 	before_create :generate_references, on: [ :create ]
-	# require 'csv'
-	#
-	# def self.send_daily_report
-	#
-	# end
+	require 'csv'
+
+	def self.send_daily_report
+		disbursements = Disbursement.where("created_at <= ? AND created_at >= ?", Date.yesterday.end_of_day, Date.yesterday.beginning_of_day)
+		disbursement_csv = CSV.generate do |csv|
+			csv << ["Id","Ext Transaction","Transaction","Currency","Amount","Phone Number","Status","Created At","Updated At","Network"]
+			disbursements.each do |disbursement|
+				csv << [ disbursement.id, disbursement.ext_transaction_id, disbursement.transaction_id, disbursement.currency, disbursement.amount, disbursement.phone_number, disbursement.status, disbursement.created_at, disbursement.updated_at, disbursement.network  ]
+			end
+		end
+
+		#call the mailer for deivery
+		DisbursementsMailer.daily_report(disbursement_csv).deliver_now
+	end
 
 	private
 	def generate_references
