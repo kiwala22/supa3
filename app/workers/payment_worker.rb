875b072c2a802894c92216dc17934f8b2f1bb7ed
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-02-20 12:42:51 +0300

    finished uploaded payments

--- app/workers/payment_worker.rb
+++ app/workers/payment_worker.rb
@@ -1,4 +1,4 @@
-class DisbursementWorker
+class PaymentWorker
 	include Sidekiq::Worker
 	sidekiq_options queue: "high"
 	sidekiq_options retry: false
@@ -6,33 +6,32 @@ class DisbursementWorker
 	require "mobile_money/mtn_ecw"
 	require "mobile_money/airtel_uganda"
 
-	def perform(gamer_id, amount, ticket_id)
-		@gamer = Gamer.find(gamer_id)
-		@ticket = Ticket.find(ticket_id)
-		@disbursement = Disbursement.new(phone_number: @gamer.phone_number, currency: "UGX", amount: amount, status: "PENDING")
-		if @gamer && @disbursement.save
-			case @gamer.phone_number
+	def perform(payment_id, amount)
+		@payment = Payment.find(payment_id)
+		@disbursement = Disbursement.new(phone_number: @payment.phone_number, currency: "UGX", amount: amount, status: "PENDING")
+		if @payment && @disbursement.save
+			case @payment.phone_number
 
 			when /^(25678|25677|25639)/
 				#process mtn disbursement
-				result = MobileMoney::MtnEcw.make_disbursement(@gamer.first_name, @gamer.last_name, @gamer.phone_number, amount, @disbursement.transaction_id)
+				result = MobileMoney::MtnEcw.make_disbursement(@payment.first_name, @payment.last_name, @payment.phone_number, amount, @disbursement.transaction_id)
 				if result
 					if result[:status] == '200'
 						#update attributes for disbusement such as the network and ticket confirmation
 						@disbursement.update_attributes(status: "SUCCESS", ext_transaction_id: result[:ext_transaction_id], network: "MTN Uganda")
-						@ticket.update_attributes(paid: true)
+						@payment.update_attributes(status: "SUCCESS")
 					else
 						@disbursement.update_attributes(status: "FAILED", ext_transaction_id: result[:ext_transaction_id], network: "MTN Uganda")
 					end
 				end
 			when /^(25675|25670)/
 				#Airtel disbursement
-				result = MobileMoney::AirtelUganda.make_disbursement(@gamer.phone_number, amount, @disbursement.transaction_id)
+				result = MobileMoney::AirtelUganda.make_disbursement(@payment.phone_number, amount, @disbursement.transaction_id)
 				if result
 					if result[:status] == '200'
 						#update attributes for disbusement such as the network and ticket confirmation
 						@disbursement.update_attributes(status: "SUCCESS", ext_transaction_id: result[:ext_transaction_id], network: "Airtel Uganda")
-						@ticket.update_attributes(paid: true)
+						@payment.update_attributes(status: "SUCCESS")
 					else
 						@disbursement.update_attributes(status: "FAILED", ext_transaction_id: result[:ext_transaction_id], network: "Airtel Uganda")
 					end
