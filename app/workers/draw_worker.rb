b79eb996c5548499d5cb57ca4cdc651bd0ea22b9
kiwala22 <martinkiwala4@gmail.com>
2020-02-20 14:20:39 +0300

    change of message for matched 3 without sequence

--- app/workers/draw_worker.rb
+++ app/workers/draw_worker.rb
@@ -101,7 +101,7 @@ class DrawWorker
          win = (ticket.amount).to_i * matched_two
          ticket.update_attributes(number_matches: number_matches, win_amount: win, paid: false, winning_number: winning_number)
          #send confirmation message
-         message_content = "CONGRATS! Your ticket #{ticket.reference} for ##{draw_id} matched #{number_matches} numbers! You've won UGX.#{win}! Winning numbers: #{draw_numbers.join("")}. Play Again to increase your entries into the Supa Jackpot"
+         message_content = "CONGRATS! Your ticket #{ticket.reference} for ##{draw_id} matched #{number_matches} numbers without sequence! You've won UGX.#{win}! Winning numbers: #{draw_numbers.join("")}. Play Again to increase your entries into the Supa Jackpot"
          SendSMS.process_sms_now(receiver: ticket.phone_number, content: message_content, sender_id: ENV['DEFAULT_SENDER_ID'])
          #process payment
          DisbursementWorker.perform_async(ticket.gamer_id, win, ticket.id)
