8826f02f93a3ae75e6904b926b635d78efefe72b
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-02-25 05:03:14 +0300

    USSD ready for production testing

--- app/workers/push_pay_broadcast_worker.rb
+++ app/workers/push_pay_broadcast_worker.rb
@@ -7,6 +7,33 @@ class PushPayBroadcastWorker
 	require "mobile_money/airtel_uganda"
 
 	def perform(broadcast_id, message)
+      @broadcast = PushPayBroadcast.find(broadcast_id)
+      phone_number = @broadcast.phone_number[3..-1]
+      data = generate_random_data()
+      data = "PUSHPAY" + data
+      transaction_id = generate_references
+      @broadcast.update_attributes(status: "PROCESSING", data: data, transaction_id: transaction_id)
+      result = MobileMoney::AirtelUganda.push_merchantpay_request(phone_number, transaction_id, message)
+      if result && result.status == 200
+         @broadcast.update_attributes(ext_transaction_id: result[:ext_transaction_id], status: "COMPLETED")
+      else
+         @broadcast.update_attributes(status: "FAILED")
+      end
 
+	end
+
+   def generate_random_data()
+      random_numbers = []
+      while random_numbers.length != 3
+           random_numbers = SecureRandom.hex(50).scan(/\d/).uniq.sample(3).map(&:to_i)
+      end
+      return random_numbers.join("")
+   end
+
+   def generate_references
+		loop do
+			transaction_id = SecureRandom.hex(10)
+			break transaction_id unless PushPayBroadcast.where(transaction_id: transaction_id).exists?
+		end
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_23_102344) do
+ActiveRecord::Schema.define(version: 2020_02_25_014135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -204,6 +204,8 @@ ActiveRecord::Schema.define(version: 2020_02_23_102344) do
     t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ext_transaction_id"
+    t.string "transaction_id"
   end
 
   create_table "reports", force: :cascade do |t|
