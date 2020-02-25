141908bf2a6d4ffdffa06a78c34c22fbc19fae78
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-02-25 23:59:10 +0300

    error caught for sms

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+   config.log_level = :debug
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
