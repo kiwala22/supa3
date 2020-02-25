67eb64ec71b101ba8220480048edaa49c159277c
kiwala22 <martinkiwala4@gmail.com>
2020-02-25 09:53:32 +0300

    fix in auto jobs controller for balance notifications

--- app/controllers/auto_jobs_controller.rb
+++ app/controllers/auto_jobs_controller.rb
@@ -81,7 +81,7 @@ class AutoJobsController < ApplicationController
       if (mtn_balance != nil || mtn_balance != false || mtn_balance != true ) && (airtel_balance != nil || airtel_balance != false || airtel_balance != true)
         @mtn_balance = mtn_balance[:amount]
         @airtel_balance = airtel_balance[:amount]
-        if @mtn_balance < threshold || @airtel_balance < threshold
+        if @mtn_balance.to_i < threshold || @airtel_balance.gsub(/,/, '').to_i < threshold
           users.each do |user|
             #send a notification message to each user
             SendSMS.process_sms_now(transaction: false, receiver: user, content: message_content, sender_id: ENV['DEFAULT_SENDER_ID'])
