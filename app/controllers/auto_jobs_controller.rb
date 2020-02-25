5c150ad6ea22c74dfc523aaf25a5a95a67cccddb
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-02-25 05:19:50 +0300

    fixed syntax in auto jobs

--- app/controllers/auto_jobs_controller.rb
+++ app/controllers/auto_jobs_controller.rb
@@ -1,77 +1,77 @@
 class AutoJobsController < ApplicationController
-    before_action :authenticate_user!, except: [:process_broadcasts, :run_predictions, :update_segments, :run_draws, :create_gamers, :update_tickets, :update_results, :update_user_info, :generate_daily_reports, :low_credit_notification]
-    skip_before_action :verify_authenticity_token
+   before_action :authenticate_user!, except: [:process_broadcasts, :run_predictions, :update_segments, :run_draws, :create_gamers, :update_tickets, :update_results, :update_user_info, :generate_daily_reports, :low_credit_notification]
+   skip_before_action :verify_authenticity_token
 
-    require 'mobile_money/mtn_ecw'
-    require 'mobile_money/airtel_uganda'
-    require 'send_sms'
+   require 'mobile_money/mtn_ecw'
+   require 'mobile_money/airtel_uganda'
+   require 'send_sms'
 
-    def process_broadcasts
-        jobs = Broadcast.where('status = ? AND execution_time <= ?', "PENDING", Time.now)
-        if !jobs.empty?
-            jobs.each do |job|
-                #first update the status of the message and then push it to the worker
-                job.update_attribute(:status, "PROCESSING")
-                BroadcastWorker.perform_async(job.id)
-            end
-        end
-        render body: nil
-    end
+   def process_broadcasts
+      jobs = Broadcast.where('status = ? AND execution_time <= ?', "PENDING", Time.now)
+      if !jobs.empty?
+         jobs.each do |job|
+            #first update the status of the message and then push it to the worker
+            job.update_attribute(:status, "PROCESSING")
+            BroadcastWorker.perform_async(job.id)
+         end
+      end
+      render body: nil
+   end
 
-    def run_predictions
-        Gamer.find_each(batch_size: 1000) do |user|
-            SegmentPredictionWorker.perform_async(user.phone_number)
-        end
-        render body: nil
-    end
+   def run_predictions
+      Gamer.find_each(batch_size: 1000) do |user|
+         SegmentPredictionWorker.perform_async(user.phone_number)
+      end
+      render body: nil
+   end
 
-    def update_segments
-        segment_summary = Gamer.order('segment ASC').group(:segment).count().except!(nil)
-        @segment = Segment.new(segment_summary.transform_keys!(&:downcase))
-        if @segment.save
-           return true
-        else
-           return false
-        end
-        render body: nil
-    end
+   def update_segments
+      segment_summary = Gamer.order('segment ASC').group(:segment).count().except!(nil)
+      @segment = Segment.new(segment_summary.transform_keys!(&:downcase))
+      if @segment.save
+         return true
+      else
+         return false
+      end
+      render body: nil
+   end
 
-    def run_draws
-        # Pick start and stop time
-        start_time = Time.now.localtime.beginning_of_minute - 10.minutes
-        end_time = Time.now.localtime
+   def run_draws
+      # Pick start and stop time
+      start_time = Time.now.localtime.beginning_of_minute - 10.minutes
+      end_time = Time.now.localtime
 
-        DrawWorker.perform_async(start_time, end_time)
-        render body: nil
-    end
+      DrawWorker.perform_async(start_time, end_time)
+      render body: nil
+   end
 
-    def create_gamers
+   def create_gamers
       GamerWorker.perform_async
       render body: nil
-    end
+   end
 
-    def update_tickets
+   def update_tickets
       UpdateTicketsWorker.perform_async
       render body: nil
-    end
+   end
 
-    def update_results
+   def update_results
       UpdateResultsWorker.perform_async
       render body: nil
-    end
+   end
 
-    def update_user_info
+   def update_user_info
       UpdateGamerInfoWorker.perform_async
       render body: nil
-    end
+   end
 
-    def generate_daily_reports
+   def generate_daily_reports
       Collection.send_daily_report
       Disbursement.send_daily_report
       render body: nil
-    end
+   end
 
-    def low_credit_notification
+   def low_credit_notification
       #check balances on both payout accounts and check if any of them is below threshold level
       threshold = 200000
       message_content = "Hello BetCity, your payout balances are currently below threshold level, please top up. Thank you."
@@ -79,15 +79,15 @@ class AutoJobsController < ApplicationController
       mtn_balance = MobileMoney::MtnEcw.get_payout_balance
       airtel_balance = MobileMoney::AirtelUganda.get_payout_balance
       if (mtn_balance != nil || mtn_balance != false || mtn_balance != true ) && (airtel_balance != nil || airtel_balance != false || airtel_balance != true)
-        @mtn_balance = mtn_balance[:amount]
-        @airtel_balance = airtel_balance[:amount]
-        if @mtn_balance < threshold || @airtel_balance < threshold
-          users.each do |user|
-            #send a notification message to each user
-            SendSMS.process_sms_now(transaction: false, receiver: user, content: message_content, sender_id: ENV['DEFAULT_SENDER_ID'])
-          end
-        end
+         @mtn_balance = mtn_balance[:amount]
+         @airtel_balance = airtel_balance[:amount]
+         if @mtn_balance < threshold || @airtel_balance < threshold
+            users.each do |user|
+               #send a notification message to each user
+               SendSMS.process_sms_now(transaction: false, receiver: user, content: message_content, sender_id: ENV['DEFAULT_SENDER_ID'])
+            end
+         end
       end
       render body: nil
-    end
+   end
 end
