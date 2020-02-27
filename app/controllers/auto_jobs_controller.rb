04d7d5826e03c91ae19d57389a72f6f1aae3cba2
kiwala22 <martinkiwala4@gmail.com>
2020-02-27 10:34:31 +0300

    removed some numbers from credit notifications

--- app/controllers/auto_jobs_controller.rb
+++ app/controllers/auto_jobs_controller.rb
@@ -75,7 +75,7 @@ class AutoJobsController < ApplicationController
       #check balances on both payout accounts and check if any of them is below threshold level
       threshold = 200000
       message_content = "Hello BetCity, your payout balances are currently below threshold level, please top up. Thank you."
-      users = ["256786481312", "256776582036", "256785724466", "256704164444", "256771870788"]
+      users = ["256786481312", "256776582036", "256785724466"]
       mtn_balance = MobileMoney::MtnEcw.get_payout_balance
       airtel_balance = MobileMoney::AirtelUganda.get_payout_balance
       if (mtn_balance != nil || mtn_balance != false || mtn_balance != true ) && (airtel_balance != nil || airtel_balance != false || airtel_balance != true)
@@ -84,7 +84,7 @@ class AutoJobsController < ApplicationController
         if @mtn_balance.to_i < threshold || @airtel_balance.gsub(/,/, '').to_i < threshold
           users.each do |user|
             #send a notification message to each user
-            SendSMS.process_sms_now(transaction: false, receiver: user, content: message_content, sender_id: ENV['DEFAULT_SENDER_ID')
+            SendSMS.process_sms_now(transaction: false, receiver: user, content: message_content, sender_id: ENV['DEFAULT_SENDER_ID'])
           end
         end
       end
