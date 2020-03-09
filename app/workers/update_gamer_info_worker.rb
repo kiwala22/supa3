b3d4d0b11822f292fb7d390dcad716f61e519547
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-03-09 20:28:16 +0300

    update old tickets created

--- app/workers/update_gamer_info_worker.rb
+++ app/workers/update_gamer_info_worker.rb
@@ -6,17 +6,20 @@ class UpdateGamerInfoWorker
    require "mobile_money/mtn_ecw"
 
    def perform()
-      Gamer.where(first_name: nil, last_name: nil).find_in_batches(batch_size: 500) do |gamers|
-         Gamer.transaction do
-            gamers.each do |gamer|
-               if gamer.phone_number =~ /^(25678|25677|25639)/
-                  info = MobileMoney::MtnEcw.get_account_info(gamer.phone_number)
-                  if info
-                     gamer.update_attributes(first_name: info[:first_name], last_name: info[:surname])
-                  end
-               end
-            end
-         end
+      # Gamer.where(first_name: nil, last_name: nil).find_in_batches(batch_size: 500) do |gamers|
+      #    Gamer.transaction do
+      #       gamers.each do |gamer|
+      #          if gamer.phone_number =~ /^(25678|25677|25639)/
+      #             info = MobileMoney::MtnEcw.get_account_info(gamer.phone_number)
+      #             if info
+      #                gamer.update_attributes(first_name: info[:first_name], last_name: info[:surname])
+      #             end
+      #          end
+      #       end
+      #    end
+      # end
+      Ticket.where.not(time: nil).find_each(batch_size: 1000) do |ticket|
+         ticket.update_attributes(created_at: ticket.time)
       end
    end
 end
