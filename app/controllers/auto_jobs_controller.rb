7b7d16f453840f7fa4c387610bef5b17beb31838
kiwala22 <martinkiwala4@gmail.com>
2020-03-16 13:56:35 +0300

    a few fixes

--- app/controllers/auto_jobs_controller.rb
+++ app/controllers/auto_jobs_controller.rb
@@ -26,13 +26,7 @@ class AutoJobsController < ApplicationController
    end
 
    def update_segments
-      segment_summary = Gamer.order('segment ASC').group(:segment).count().except!(nil)
-      @segment = Segment.new(segment_summary.transform_keys!(&:downcase))
-      if @segment.save
-         return true
-      else
-         return false
-      end
+      Segment.update_segments
       render body: nil
    end
 
--- app/models/segment.rb
+++ app/models/segment.rb
@@ -1,3 +1,12 @@
 class Segment < ApplicationRecord
-
+  #method to create segments
+  def self.update_segments
+    segment_summary = Gamer.order('segment ASC').group(:segment).count().except!(nil)
+    @segment = Segment.new(segment_summary.transform_keys!(&:downcase))
+    if @segment.save
+       return true
+    else
+       return false
+    end
+  end
 end
