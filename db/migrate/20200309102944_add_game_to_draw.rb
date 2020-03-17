82d5bcd450b119496fc7273edbab393459480116
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-03-17 16:31:43 +0300

    added merge comments

--- db/migrate/20200309102944_add_game_to_draw.rb
+++ db/migrate/20200309102944_add_game_to_draw.rb
@@ -1,9 +1,6 @@
 class AddGameToDraw < ActiveRecord::Migration[5.2]
   def change
     add_column :draws, :game, :string, default: "Supa3"
-<<<<<<< HEAD
     add_index :draws, :game
-=======
->>>>>>> origin/feature_updates
   end
 end
