2ef3e4059e3f52fde583c5d01a74dadb9ec26c86
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-03-19 12:20:20 +0300

    sidekiq initializer ignored and located in shared folder

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,14 @@
 require 'sidekiq'
 require 'sidekiq/web'
-require "sidekiq/throttled"
-Sidekiq::Throttled.setup!
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   [user, password] == ["admin", "admin2013"]
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = { url: 'redis://127.0.0.1:6379/1' }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: 'redis://127.0.0.1:6379/1' }
 end
