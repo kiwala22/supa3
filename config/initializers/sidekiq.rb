e639f65c02ab3a6980c6e9d5ad8fde8296b6ca9b
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-03-19 12:32:31 +0300

    updated sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,9 +6,9 @@ Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/1' }
+  config.redis = { url: 'redis://127.0.0.1:6379/0' }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/1' }
+  config.redis = { url: 'redis://127.0.0.1:6379/0' }
 end
