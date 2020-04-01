710deb03105fe420c99ace0da2822bdbf06df009
Acacia Bengo Ssembajjwe <acaciabengo@gmail.com>
2020-04-02 00:12:52 +0300

    chnaged mtn workflow

--- app/controllers/confirmation/mtn_uganda_controller.rb
+++ app/controllers/confirmation/mtn_uganda_controller.rb
@@ -23,9 +23,15 @@ class Confirmation::MtnUgandaController < ApplicationController
 			if @transaction.save
 				MtnCollectionWorker.perform_async(@transaction.transaction_id, @transaction.ext_transaction_id, "SUCCESS")
 			else
-				MtnCollectionWorker.perform_async(@transaction.transaction_id, @transaction.ext_transaction_id, "FAILED")
-				#log the error
-				@@logger.error(@transaction.errors.full_messages)
+				#check if it is existing
+				collecton = Collection.find_by(ext_transaction_id: @transaction.ext_transaction_id)
+				if collection.present?
+					MtnCollectionWorker.perform_async(collecton.transaction_id, collecton.ext_transaction_id, "EXISTING")
+				else
+					MtnCollectionWorker.perform_async(@transaction.transaction_id, @transaction.ext_transaction_id, "FAILED")
+					#log the error
+					@@logger.error(@transaction.errors.full_messages)
+				end
 			end
 			render xml: "<?xml version='1.0' encoding='UTF-8'?><ns0:paymentresponse xmlns:ns0='http://www.ericsson.com/em/emm/sp/backend'><providertransactionid>#{@transaction.ext_transaction_id}</providertransactionid><message>pending</message><status>PENDING</status></ns0:paymentresponse>"
 		end
--- app/workers/mtn_collection_worker.rb
+++ app/workers/mtn_collection_worker.rb
@@ -16,9 +16,9 @@ class MtnCollectionWorker
 	def perform(transaction_id, ext_transaction_id, status)
 		@username = ENV['COLLECTION_USERNAME']
 		@password = ENV['COLLECTION_PASSWORD']
-		@collection =  Collection.find_by(transaction_id: transaction_id)
-		if @collection && @collection.status != "SUCCESSFUL"
-			if status == 'SUCCESS'
+		if status == 'SUCCESS'
+			@collection =  Collection.find_by(transaction_id: transaction_id)
+			if @collection && @collection.status != "SUCCESSFUL"
 				#send request and mark status as successful
 				url = "https://f5.mtn.co.ug:8006/poextvip/v1/paymentcompleted"
 				req_xml = "<?xml version='1.0' encoding='UTF-8'?><ns4:paymentcompletedrequest xmlns:ns4='http://www.ericsson.com/em/emm/serviceprovider/v1_0/backend' xmlns:op='http://www.ericsson.com/em/emm/v1_0/common' xmlns:xs='http://www.w3.org/2001/XMLSchema' version='1.0'><transactionid>#{@collection.ext_transaction_id}</transactionid><providertransactionid>#{@collection.transaction_id}</providertransactionid><status>COMPLETED</status></ns4:paymentcompletedrequest>"
@@ -42,8 +42,47 @@ class MtnCollectionWorker
 					TicketWorker.perform_async(@collection.phone_number,@collection.message, @collection.amount)
 				end
 			end
+		elsif status == 'EXISTING'
+			@collection =  Collection.find_by(transaction_id: transaction_id)
+			#send request and mark status as successful
+			url = "https://f5.mtn.co.ug:8006/poextvip/v1/paymentcompleted"
+			req_xml = "<?xml version='1.0' encoding='UTF-8'?><ns4:paymentcompletedrequest xmlns:ns4='http://www.ericsson.com/em/emm/serviceprovider/v1_0/backend' xmlns:op='http://www.ericsson.com/em/emm/v1_0/common' xmlns:xs='http://www.w3.org/2001/XMLSchema' version='1.0'><transactionid>#{@collection.ext_transaction_id}</transactionid><providertransactionid>#{@collection.transaction_id}</providertransactionid><status>COMPLETED</status></ns4:paymentcompletedrequest>"
+			uri = URI.parse(url)
+			http = Net::HTTP.new(uri.host, uri.port)
+			request = Net::HTTP::Post.new(uri.request_uri)
+			request.basic_auth(@username, @password)
+			request.content_type = 'text/xml'
+			request.body = req_xml
+			http.use_ssl = true
+			http.ssl_version = :TLSv1_2
+			http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+			http.cert = OpenSSL::X509::Certificate.new(File.read(Rails.root.join("config/prod/134_209_22_183.crt")))
+			http.key = http.key = OpenSSL::PKey::RSA.new(File.read(Rails.root.join("config/prod/134_209_22_183.key")))
+			http.ca_file = Rails.root.join("config/prod/m3_EXTERNAL_ca_UG.crt").to_s
+			http.set_debug_output($stdout)
+			res = http.request(request)
 
+		elsif == 'FAILED'
+			@collection =  Collection.find_by(transaction_id: transaction_id)
+			#send request and mark status as successful
+			url = "https://f5.mtn.co.ug:8006/poextvip/v1/paymentcompleted"
+			req_xml = "<?xml version='1.0' encoding='UTF-8'?><ns4:paymentcompletedrequest xmlns:ns4='http://www.ericsson.com/em/emm/serviceprovider/v1_0/backend' xmlns:op='http://www.ericsson.com/em/emm/v1_0/common' xmlns:xs='http://www.w3.org/2001/XMLSchema' version='1.0'><transactionid>#{ext_transaction_id}</transactionid><providertransactionid>#{transaction_id}</providertransactionid><status>FAILED</status></ns4:paymentcompletedrequest>"
+			uri = URI.parse(url)
+			http = Net::HTTP.new(uri.host, uri.port)
+			request = Net::HTTP::Post.new(uri.request_uri)
+			request.basic_auth(@username, @password)
+			request.content_type = 'text/xml'
+			request.body = req_xml
+			http.use_ssl = true
+			http.ssl_version = :TLSv1_2
+			http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+			http.cert = OpenSSL::X509::Certificate.new(File.read(Rails.root.join("config/prod/134_209_22_183.crt")))
+			http.key = http.key = OpenSSL::PKey::RSA.new(File.read(Rails.root.join("config/prod/134_209_22_183.key")))
+			http.ca_file = Rails.root.join("config/prod/m3_EXTERNAL_ca_UG.crt").to_s
+			http.set_debug_output($stdout)
+			res = http.request(request)
 		end
+
 	rescue StandardError => e
 		@@logger.error(e.message)
 		false
