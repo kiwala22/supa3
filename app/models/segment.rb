6e8fcb4b427dd8ca7e434af0aae66fedbadf76a8
kiwala22 <martinkiwala4@gmail.com>
2020-06-12 14:18:04 +0300

    fix for segment creation

--- app/models/segment.rb
+++ app/models/segment.rb
@@ -4,6 +4,7 @@ class Segment < ApplicationRecord
     segment_summary = Gamer.order('segment ASC').group(:segment).count().except!(nil)
     @segment = Segment.new(segment_summary.transform_keys!(&:downcase))
     if @segment.save
+       @segment.update_attributes(created_at: Time.now)
        return true
     else
        return false
