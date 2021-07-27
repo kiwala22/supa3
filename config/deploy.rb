15044cf7ff1f11a5537981452174faf2f70a71ac
martin kiwala <martinkiwala4@gmail.com>
2021-07-27 23:02:23 +0300

    change in deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "~> 3.16.0"
 
 set :application, "SupaAI"
 set :repo_url, "git@bitbucket.org:skylinesmslimited/supa_ai.git"
